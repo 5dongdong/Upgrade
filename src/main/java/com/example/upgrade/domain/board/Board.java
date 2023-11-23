b83a6d459d19c475100fe1a7fb0e0cb9e09009bf
Donghyun Oh <dhgus44@naver.com>
2023-11-23 19:09:45 +0900

    feat:board작성

--- src/main/java/com/example/upgrade/domain/board/Board.java
+++ src/main/java/com/example/upgrade/domain/board/Board.java
@@ -1,4 +1,20 @@
 package com.example.upgrade.domain.board;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.Getter;
+
+@Entity
+@Getter
 public class Board {
+
+    @Id
+    @Column(name = "board_Id")
+    Long boardId;
+
+    @Lob
+    @Column(name = "board_content")
+    String content;
 }
